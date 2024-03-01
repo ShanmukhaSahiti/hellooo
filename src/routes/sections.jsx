d02b861eb6236149b686dd032ec2d79180128e39
Shanmukha Sahiti Challa <shanmukha.96@gmail.com>
2024-03-02 04:21:40 +0530

    trying diff base in viteconfig

--- src/routes/sections.jsx
+++ src/routes/sections.jsx
@@ -5,10 +5,10 @@ import DashboardLayout from 'src/layouts/dashboard';
 
 export const IndexPage = lazy(() => import('src/pages/app'));
 export const ChatViewPage = lazy(() => import('src/pages/chat-view'));
-export const BlogPage = lazy(() => import('src/pages/blog'));
-export const UserPage = lazy(() => import('src/pages/user'));
-export const LoginPage = lazy(() => import('src/pages/login'));
-export const ProductsPage = lazy(() => import('src/pages/products'));
+// export const BlogPage = lazy(() => import('src/pages/blog'));
+// export const UserPage = lazy(() => import('src/pages/user'));
+// export const LoginPage = lazy(() => import('src/pages/login'));
+// export const ProductsPage = lazy(() => import('src/pages/products'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
 // ----------------------------------------------------------------------
